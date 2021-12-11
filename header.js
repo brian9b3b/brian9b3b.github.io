f7a2b710a849400226811730f3a9e134abed5f29
brian9b3b <halladay2407@gmail.com>
2021-12-11 23:52:43 +0800

    1211

---
+++ header.js
@@ -0,0 +1,17 @@
+
+<div>\
+
+	<h1>\<font face="monospace">\Brina_lee Demo web</font>\</h1>\</div>\
+	/*<img src="head_img.jpeg" alt="替代文字一" title="範例圖片一" width="700px" height="500px">
+    <header class="main-header">
+		<div class="container">
+		
+			<nav class="navbar">
+				<ul class="main-menu">
+				<li><a href="#">關於我</a></li>
+				<li><a href="#">作品</a></li>
+				<li><a href="#">聯絡我</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>*/ 
